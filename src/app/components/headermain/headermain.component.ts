9221f120cbe5dd225bb858bcc49f6989be626443
Raman <ramanthakur13135@gmail.com>
2025-01-01 11:51:41 +0530

    fixes

--- src/app/components/headermain/headermain.component.ts
+++ src/app/components/headermain/headermain.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { AuthService } from '../../services/auth.service';
+import { RouteServiceService } from '../../services/route-service.service';
+import { UtilService } from '../../services/util.service';
 
 @Component({
   selector: 'app-headermain',
@@ -8,5 +14,19 @@ import { Component } from '@angular/core';
   styleUrl: './headermain.component.css'
 })
 export class HeadermainComponent {
-
+    pchangeForm!: FormGroup;
+  constructor(private router: Router,
+     private authService:AuthService,
+     private routeService:RouteServiceService,
+ 
+     private utils:UtilService,
+     private spinner: NgxSpinnerService,
+     private fb: FormBuilder,
+   ) {
+    
+   }
+  logout() {
+  
+    this.authService.logout();
+  }
 }
